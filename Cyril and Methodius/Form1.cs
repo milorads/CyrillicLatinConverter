689c13ec9b62f9bb8ad3b5accfba49d438f404e2
msekulovic <milorad.sekulovic@gmail.com>
2016-11-18 16:55:54 +0100

    added button to choose file

--- Cyril and Methodius/Form1.cs
+++ Cyril and Methodius/Form1.cs
@@ -8,6 +8,7 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 using Cyril_and_Methodius.Model;
+using System.IO;
 
 namespace Cyril_and_Methodius
 {
@@ -16,14 +17,38 @@ namespace Cyril_and_Methodius
         public Form1()
         {
             // test
-            string a = "Тест ћирилична метода, са још по неким карактером. ХЕ ЏА џ xzwq QQ // џ њ љ Џ Њ Љ";
-            string b = "Теst latinična metoda, sa još po nekim karakterom. Xe dža Dž DŽ xzwq QQ // dŽ dž Dž DŽ Nj nj NJ nJ lj lJ LJ Lj";
-            Translator t = new Translator();
-            string outS = Translator.Translate(a, true);
-            string outS2 = Translator.Translate(b, false);
-            Console.Read();
+            //string a = "Тест ћирилична метода, са још по неким карактером. ХЕ ЏА џ xzwq QQ // џ њ љ Џ Њ Љ";
+            //string b = "Теst latinična metoda, sa još po nekim karakterom. Xe dža Dž DŽ xzwq QQ // dŽ dž Dž DŽ Nj nj NJ nJ lj lJ LJ Lj";
+            //Translator t = new Translator();
+            //string outS = Translator.Translate(a, true);
+            //string outS2 = Translator.Translate(b, false);
+            //Console.Read();
             // test
             InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog fDialog = new OpenFileDialog();
+            fDialog.Title = "Open XML/UML File";
+            fDialog.Filter = "XML Files|*.txt";
+            fDialog.InitialDirectory = @"C:\";
+            int size = -1;
+            DialogResult result = fDialog.ShowDialog(); // Show the dialog.
+            if (result == DialogResult.OK) // Test result.
+            {
+                string file = fDialog.FileName;
+                try
+                {
+                    string text = File.ReadAllText(file);
+                    size = text.Length;
+                }
+                catch (IOException)
+                {
+                }
+            }
+            Console.WriteLine(size); // <-- Shows file size in debugging mode.
+            Console.WriteLine(result); // <-- For debugging use.
         }
     }
 }
